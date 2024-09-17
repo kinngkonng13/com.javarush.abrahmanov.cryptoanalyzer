a35255b7303197f1c11bee2b43fad5b61978e895
Arman <king.nokongo@gmail.com>
2024-09-17 15:52:49 +0600

    Commit

---
+++ App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\t\tШИФР ЦЕЗАРЯ");
+        System.out.println("Выберите действие");
+        System.out.println("1. Считать адрес файла с которого будет считан текст");
+        System.out.println("2. Считать адрес файла в который будет записан текст");
+        System.out.println("3. Ввести ключ");
+        System.out.println("4. Выход");
+
+    }
+}
---
+++ Cipher.java
@@ -0,0 +1,5 @@
+public class Cipher {
+    private char[] alphabet;
+    public Cipher(char[] alphabet) {
+        this.alphabet = alphabet;
+}
